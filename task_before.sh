355d5a23c177d043c48840e0c7ce11786e7d2e0a
442774765 <74840658+442774765@users.noreply.github.com>
2021-08-04 07:48:09 +0800

    Add files via upload

--- task_before.sh
+++ task_before.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Build 20210727-001
+
 ## 东东农场：
 MyFruit1=''
 
@@ -90,53 +92,108 @@ MyCity1=''
 ForOtherCity1=""
 
 
+## 摇钱树：
+MyMoneyTree1=''
+
+ForOtherMoneyTree1=""
+
+
+## 京喜Token(用于京喜财富岛提现等)
+TokenJxnc1=''
+
+
 env_name=(
-  FRUITSHARECODES
-  PETSHARECODES
-  PLANT_BEAN_SHARECODES
-  DREAM_FACTORY_SHARE_CODES
-  DDFACTORY_SHARECODES
-  JDJOY_SHARECODES
-  JDZZ_SHARECODES
-  JXNC_SHARECODES
-  BOOKSHOP_SHARECODES
-  JD_CASH_SHARECODES
-  JDSGMH_SHARECODES
-  JDCFD_SHARECODES
-  JDHEALTH_SHARECODES
+  FRUITSHARECODES                     ## 1、东东农场互助码
+  PETSHARECODES                       ## 2、东东萌宠互助码
+  PLANT_BEAN_SHARECODES               ## 3、种豆得豆互助码
+  DREAM_FACTORY_SHARE_CODES           ## 4、京喜工厂互助码
+  DDFACTORY_SHARECODES                ## 5、东东工厂互助码
+  JDJOY_SHARECODES                    ## 6、疯狂的JOY互助码
+  JDZZ_SHARECODES                     ## 7、京东赚赚互助码
+  JXNC_SHARECODES                     ## 8、京喜农场助力码
+  BOOKSHOP_SHARECODES                 ## 9、口袋书店互助码
+  JD_CASH_SHARECODES                  ## 10、签到领现金互助码
+  JDSGMH_SHARECODES                   ## 11、闪购盲盒互助码
+  JDCFD_SHARECODES                    ## 12、京喜财富岛互助码
+  JDHEALTH_SHARECODES                 ## 13、东东健康社区互助码
+  CITY_SHARECODES                     ## 14、城城领现金互助码
+  JD818_SHARECODES                    ## 15、京东手机狂欢城互助码
+  MONEYTREE_SHARECODES                ## 16、摇钱树
+  JXNCTOKENS                          ## 17、京喜Token(京喜财富岛提现用)
 )
 var_name=(
-  ForOtherFruit
-  ForOtherPet
-  ForOtherBean
-  ForOtherDreamFactory
-  ForOtherJdFactory
-  ForOtherJoy
-  ForOtherJdzz
-  ForOtherJxnc
-  ForOtherBookShop
-  ForOtherCash
-  ForOtherSgmh
-  ForOtherCfd
-  ForOtherHealth
+  ForOtherFruit                       ## 1、东东农场互助规则
+  ForOtherPet                         ## 2、东东萌宠互助规则
+  ForOtherBean                        ## 3、种豆得豆互助规则
+  ForOtherDreamFactory                ## 4、京喜工厂互助规则
+  ForOtherJdFactory                   ## 5、东东工厂互助规则
+  ForOtherJoy                         ## 6、疯狂的JOY互助规则
+  ForOtherJdzz                        ## 7、京东赚赚互助规则
+  ForOtherJxnc                        ## 8、京喜农场助力码
+  ForOtherBookShop                    ## 9、口袋书店互助规则
+  ForOtherCash                        ## 10、签到领现金互助规则
+  ForOtherSgmh                        ## 11、闪购盲盒互助规则
+  ForOtherCfd                         ## 12、京喜财富岛互助规则
+  ForOtherHealth                      ## 13、东东健康社区互助规则
+  ForOtherCarni                       ## 14、城城领现金互助规则
+  ForOtherCity                        ## 15、京东手机狂欢城互助规则
+  ForOtherMoneyTree                   ## 16、摇钱树
+  TokenJxnc                           ## 17、京喜Token(京喜财富岛提现用)
 )
 
-combine_sub() {
-    local what_combine=$1
-    local combined_all=""
-    local tmp1 tmp2
+## 临时屏蔽某账号运行活动脚本
+TempBlock_JD_COOKIE(){
+    source $file_env
+    local TempBlockCookieInterval=$(echo $TempBlockCookie | perl -pe "{s|~|-|; s|_|-|}" | sed 's/\(\d\+\)-\(\d\+\)/{\1..\2}/g')
+    local TempBlockCookieArray=($(eval echo $TempBlockCookieInterval))
     local envs=$(eval echo "\$JD_COOKIE")
     local array=($(echo $envs | sed 's/&/ /g'))
     local user_sum=${#array[*]}
-    for ((i = 1; i <= $user_sum; i++)); do
+    local m n t
+    for ((m = 1; m <= $user_sum; m++)); do
+        n=$((m - 1))
+        for ((t = 0; t < ${#TempBlockCookieArray[*]}; t++)); do
+            [[ "${TempBlockCookieArray[t]}" = "$m" ]] && unset array[n]
+        done
+    done
+    jdCookie=$(echo ${array[*]} | sed 's/\ /\&/g')
+    [[ ! -z $jdCookie ]] && export JD_COOKIE="$jdCookie"
+    temp_user_sum=${#array[*]}
+}
+
+## 组合互助码格式化为全局变量的函数
+combine_sub() {
+    source $file_env
+    local what_combine=$1
+    local combined_all=""
+    local tmp1 tmp2
+    local TempBlockCookieInterval=$(echo $TempBlockCookie | perl -pe "{s|~|-|; s|_|-|}" | sed 's/\(\d\+\)-\(\d\+\)/{\1..\2}/g')
+    local TempBlockCookieArray=($(eval echo $TempBlockCookieInterval))
+    local envs=$(eval echo "\$JD_COOKIE")
+    local array=($(echo $envs | sed 's/&/ /g'))
+    local user_sum=${#array[*]}
+    local a b i j t sum
+    for ((i=1; i <= $user_sum; i++)); do
         local tmp1=$what_combine$i
+        local tmp2=${!tmp1}
+        [[ ${tmp2} ]] && sum=$i || break
+    done
+    for ((j = 1; j <= $user_sum; j++)); do
+        a=$temp_user_sum
+        b=$sum
+        if [[ $a -ne $b ]]; then
+            for ((t = 0; t < ${#TempBlockCookieArray[*]}; t++)); do
+                [[ "${TempBlockCookieArray[t]}" = "$j" ]] && continue 2
+            done
+        fi
+        local tmp1=$what_combine$j
         local tmp2=${!tmp1}
         combined_all="$combined_all&$tmp2"
     done
     echo $combined_all | perl -pe "{s|^&||; s|^@+||; s|&@|&|g; s|@+&|&|g; s|@+|@|g; s|@+$||}"
 }
 
-## 正常依次运行时，组合所有账号的Cookie与互助码
+## 正常依次运行时，组合互助码格式化为全局变量
 combine_all() {
     for ((i = 0; i < ${#env_name[*]}; i++)); do
         result=$(combine_sub ${var_name[i]})
@@ -146,8 +203,10 @@ combine_all() {
     done
 }
 
-if [[ $(ls $dir_code) ]]; then
-    latest_log=$(ls -r $dir_code | head -1)
-    . $dir_code/$latest_log
+TempBlock_JD_COOKIE
+
+#if [[ $(ls $dir_code) ]]; then
+#    latest_log=$(ls -r $dir_code | head -1)
+#    . $dir_code/$latest_log
     combine_all
-fi
+#fi
